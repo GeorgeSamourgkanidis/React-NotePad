48fa91259bdb3c9ecc3a2195feb7e546bbff61d1
Samou <50735816+dai18058@users.noreply.github.com>
2021-04-04 19:03:52 +0300

    Add files via upload

---
+++ precache-manifest.f9f1243a9125423baf5d3e647d80295f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51df48e17e8a8806b9ca28157b2ff995",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f3fa5ac507983a3b476d",
+    "url": "./static/js/2.b6ca354c.chunk.js"
+  },
+  {
+    "revision": "6d201ed4936cf84730fe",
+    "url": "./static/js/main.d9a6fa7d.chunk.js"
+  },
+  {
+    "revision": "7dd1b07ef8cef8e5718b",
+    "url": "./static/js/runtime-main.79c9e6b2.js"
+  }
+]);
